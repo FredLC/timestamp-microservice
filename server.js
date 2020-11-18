2bd8ca6780a27afd84a60e7e66fa58f0120bb2a1
fredlc <fredlefevre@outlook.com>
2020-11-18 18:39:39 -0500

    Implementation of timestamp microservice

--- server.js
+++ server.js
@@ -1,32 +1,40 @@
-// server.js
-// where your node app starts
-
-// init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 }));
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-// http://expressjs.com/en/starter/basic-routing.html
+app.get("/api/timestamp/:date?", function (req, res) {
+  if (req.params.date) {
+    let dateInput = req.params.date;
+    if (!isNaN(dateInput)) {
+      dateInput = Number(dateInput);
+    }
+    if (new Date(dateInput).toLocaleDateString() === "Invalid Date") {
+      res.json({ error: "Invalid Date" });
+    }
+    if (isNaN(dateInput)) {
+      let date = new Date(dateInput);
+      let utc = date.toUTCString();
+      let unix = date.getTime();
+      res.json({ unix: unix, utc: utc });
+    } else {
+      let date = new Date(dateInput);
+      date = date.toUTCString();
+      res.json({ unix: dateInput, utc: date });
+    }
+  } else {
+    let date = new Date();
+    res.json({ unix: Date.now(), utc: date.toUTCString() });
+  }
+});
+
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
-
-
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+app.listen(3000, function () {
+  console.log("Your app is listening on port 3000");
 });
